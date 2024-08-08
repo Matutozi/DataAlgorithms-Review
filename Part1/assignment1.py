26c0fdef508c91db314bf690119a4bb1fbb858b9
Matutozi <sobowalegz2@gmail.com>
2024-08-08 14:03:18 +0100

    added the complete implementation using binary search,'

--- Part1/assignment1.py
+++ Part1/assignment1.py
@@ -29,7 +29,36 @@ def count_rotation_linear(nums):
         position += 1
     return 0
 
-
+"""
 from jovian.pythondsa import evaluate_test_case
 evaluate_test_case(count_rotation_linear, test)
 #to test it out
+"""
+
+"""time complexity : O(N)
+"""
+
+"""USING BINARY SEARCH"""
+def count_rotations_binary(nums):
+    """method that implements the solution using binary search"""
+    lo = 0
+    hi = len(nums) - 1
+
+    while lo <= hi:
+        mid = (lo + hi) // 2
+        mid_number = nums[mid]
+
+        if mid > 0 and mid_number < nums[mid - 1]:
+            return mid
+        
+        if mid < len(nums) - 1 and nums[mid + 1] < nums[mid]:
+            return mid +1
+        
+        if nums[mid] >= nums[lo]:
+            lo = lo + 1
+
+        else:
+            hi = hi - 1
+
+from jovian.pythondsa import evaluate_test_case
+evaluate_test_case(count_rotations_binary, test)
