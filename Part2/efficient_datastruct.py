072bf1abb3328bb3f636de43a0444abe62872807
Matutozi <sobowalegz2@gmail.com>
2024-08-09 21:18:05 +0100

    method that implements the pre order and post order transversal

--- Part2/efficient_datastruct.py
+++ Part2/efficient_datastruct.py
@@ -61,9 +61,9 @@ print(tree2.right.right.right.key)
 
 """TRAVERSING A BINARY TREE
 moving through an element of a binary tree once
-- inorder LMR
-- preorder
-- post order
+- inorder LNR
+- preorder NLR
+- post order LRN
 """
 
 #INORDER TRAVERSAL
@@ -74,3 +74,21 @@ def traverse_in_order(node):
     return (traverse_in_order(node.left) + [node.key] + traverse_in_order(node.right))
 
 print(traverse_in_order(tree2))
+
+#PRE ORDER TRANSVERSAL
+def transverse_pre_order(node):
+    """METHOD THAT IMPLEMETS PRE ORDER TRANSVERSAL"""
+    if node is None:
+        return []
+    return ([node.key] + transverse_pre_order(node.left) + transverse_pre_order(node.right))
+
+print(transverse_pre_order(tree2))
+
+#POST ORDER TRANSVERSAl
+def traverse_post_order(node):
+    """Method that implements post-order traversal."""
+    if node is None:
+        return []
+    return traverse_post_order(node.left) + traverse_post_order(node.right) + [node.key]
+
+print(traverse_post_order(tree2))
