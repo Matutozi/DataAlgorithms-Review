e78a1fde422a60c47405278cf0e2ce5298ee59d8
Matutozi <sobowalegz2@gmail.com>
2024-08-16 00:05:19 +0100

    DAY 2: Greatest Common Divisor of Strings

---
+++ leetcode75/prob_1071.py
@@ -0,0 +1,18 @@
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        #to write this code we need an helper function that finds the gcd
+        def gcd(a,b):
+            while b:
+                a, b = b, a % b
+            return a
+
+        str1_len = len(str1)
+        str2_len = len(str2)
+        length_gcd = gcd(str1_len, str2_len)
+
+        candidate = str1[:length_gcd]
+
+        if candidate * (str1_len // length_gcd) == str1 and candidate * (str2_len // length_gcd)== str2:
+            return candidate
+        else:
+            return ""
