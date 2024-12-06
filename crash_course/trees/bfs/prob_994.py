18b28b67c3a028fd5d9905af18c461f2d3de6ad6
matutozi <sobowalegz2@gmail.com>
2024-12-06 13:39:49 +0100

    feat: problems on bfs binary trees

---
+++ crash_course/trees/bfs/prob_994.py
@@ -0,0 +1,45 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isCousins(self, root: Optional[TreeNode], x: int, y: int) -> bool:
+        """to find if x and y are cousins, if they are cousins they would have same depth but different parents"""
+        if not root:
+            return False
+
+        if root is not None:
+            t_queue = collections.deque([(root, None)]) #to store the node, parent
+        
+        while t_queue:
+            size = len(t_queue)
+
+            x_parent=None
+            y_parent = None
+
+            for i in range(size):
+                node, parent = t_queue.popleft()
+
+                if node.val == x:
+                    x_parent = parent
+                if node.val == y:
+                    y_parent = parent
+
+                if node.left:
+                    t_queue.append((node.left, node))
+                if node.right:
+                    t_queue.append((node.right, node))
+
+            if x_parent and y_parent:
+                return x_parent != y_parent
+            
+            if x_parent or y_parent:
+                return False
+        return False
+
+
+
+
+
